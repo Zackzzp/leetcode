8024a36816d953f8870d23f3e0ad9697adae5402
zackzzp <1113739573@qq.com>
2024-10-14 11:21:51 +0800

    手撕接雨水！！！！

--- src/Trap.java
+++ src/Trap.java
@@ -1,3 +1,4 @@
+import java.nio.file.StandardOpenOption;
 import java.util.*;
 
 public class Trap {
@@ -23,6 +24,48 @@ public class Trap {
 
     //单调栈
     public int trap1(int[] height) {
-
+        int ans = 0;
+        Deque<Integer> stack = new LinkedList<>();
+        int n = height.length;
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                int top = stack.pop();
+                if (stack.isEmpty())
+                    break;
+                int left = stack.peek();
+                int currWidth = i - left - 1;
+                int currHeight = Math.min(height[left], height[i] )- height[top];
+                ans += currWidth * currHeight;
+            }
+            stack.push(i);
+        }
+        return ans;
     }
+    //动态规划
+    public int trap3(int[] height) {
+        int n = height.length;
+        if (n == 0) {
+            return 0;
+        }
+
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; ++i) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return ans;
+    }
+
+
 }
